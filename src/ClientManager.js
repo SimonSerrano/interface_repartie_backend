15bc3ec7e133b87d929a121610b60dc6c4ca1c8c
SimonSerrano <simon.serrano@hotmail.fr>
2020-01-17 11:56:12 +0100

    Added server redirection of events

--- src/ClientManager.js
+++ src/ClientManager.js
@@ -2,7 +2,7 @@
  * @author Christian Brel <ch.brel@gmail.com>
  */
 
-import { PING_SOCKETIO_TYPE, PONG_SOCKETIO_TYPE } from './constants';
+import { PING_SOCKETIO_TYPE, PONG_SOCKETIO_TYPE, SERVER_REDIRECTS } from './constants';
 
 /**
  * Main class to manage ClientManager.
@@ -30,6 +30,12 @@ class ClientManager {
       console.log(`Received PONG from ${data.id}!`);
     });
     socket.emit(PING_SOCKETIO_TYPE, { id: socket.id });
+    SERVER_REDIRECTS.forEach(redirect => {
+      socket.on(redirect, data => {
+      console.log(`Received ${redirect} from ${data.id}`);
+      socket.emit(redirect, {id: data.id});
+      });
+    });
   }
 
   /**
